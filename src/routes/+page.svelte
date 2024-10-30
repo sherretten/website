850c8d75773e858ba8b3bfd7237a629534b66663
Nate <nathaniels2016@gmail.com>
2024-10-30 15:58:46 -0700

    found initial design idea

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<header>
+	<h1 class="name">Nate Sherrett</h1>
+	<h2>Software Engineer</h2>
+	<p></p>
+	<nav>
+		<a href="#about">About</a>
+		<a href="#experience">Experience</a>
+		<a href="#projects">Projects</a>
+	</nav>
+</header>
+
+<main>
+	<section id="about">
+		<div>Hi I'm Nate, I'm a Software Engineer based out of Bend, OR.</div>
+	</section>
+
+	<section id="experience">
+		<div>
+			<div class="job-container">
+				<h1>Nordic Semiconductor</h1>
+				<div class="title">Senior Frontend Engineer</div>
+				<div class="date">June 2021 - October 2024</div>
+				<div class="stack">React, Typescript, Redux, Graphql</div>
+				<div class="description"></div>
+			</div>
+		</div>
+		<div class="projects-container">
+			<div class="codewithfriends"></div>
+		</div>
+	</section>
+	<section id="projects"></section>
+</main>
+
+<style>
+	.job-container {
+		background-color: white;
+		color: black;
+	}
+
+	section {
+		width: 100%;
+		background-color: purple;
+	}
+	a {
+		color: inherit;
+		text-decoration: none;
+		font-size: 1.5em;
+	}
+	a:visited {
+		text-decoration: none;
+		color: inherit;
+	}
+	a:active {
+		border: 1px solid goldenrod;
+	}
+</style>
