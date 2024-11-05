e8090f5ce30ec60c8c77e7f31c00c39ae7deae16
Nate <nathaniels2016@gmail.com>
2024-11-05 15:15:46 -0800

    Add section boxes, add links, update linkedin svg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,266 +1,322 @@
 <div class="container">
-	<div class="wrapper">
-		<header>
-			<div class="base-content">
-				<h1 class="name">Nate Sherrett</h1>
-				<h2>Software Engineer</h2>
-				<p>I enjoy writing code and solving problems</p>
-				<nav>
-					<a class="section-link" href="#about">About</a>
-					<a class="section-link" href="#experience">Experience</a>
-					<a class="section-link" href="#projects">Projects</a>
-				</nav>
-			</div>
+  <div class="wrapper">
+    <header>
+      <div class="base-content">
+        <h1 class="name">Nate Sherrett</h1>
+        <h2>Software Engineer</h2>
+        <p>I enjoy writing code and solving problems</p>
+        <nav>
+          <a class="section-link" href="#about">About</a>
+          <a class="section-link" href="#experience">Experience</a>
+          <a class="section-link" href="#projects">Projects</a>
+        </nav>
+      </div>
 
-			<div class="outer-links">
-				<a href="https://github.com/sherretten" target="_blank">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						role="img"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						class="icon"
-						><title>GitHub</title><path
-							d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
-						></path></svg
-					>
-				</a>
+      <div class="outer-links">
+        <a href="https://github.com/sherretten" target="_blank" rel="noreferrer noopener">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon">
+						<title>GitHub</title>
+						<path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/>
+					</svg>
+        </a>
 
-				<a
-					href="https://www.linkedin.com/in/nathaniel-sherrett-7a4aa0166/"
-					target="_blank"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						role="img"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						class="icon"
-						><title>LinkedIn</title><path
-							d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
-						></path><rect x="2" y="9" width="4" height="12"></rect><circle
-							cx="4"
-							cy="4"
-							r="2"
-						></circle></svg
-					>
-				</a>
-			</div>
-		</header>
+        <a
+          href="https://www.linkedin.com/in/nathaniel-sherrett-7a4aa0166/"
+          target="_blank"
+					rel="noreferrer noopener">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon"
+            >
+						<title>LinkedIn</title>
+            <path
+              d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"/>
+					</svg>
+        </a>
+      </div>
+    </header>
 
-		<!-- — -->
-		<main>
-			<section id="about">
-				<h2 class="section-title">About</h2>
-				<div>Hi I'm Nate, I'm a Software Engineer based out of Bend, OR.</div>
-				<p>
-					I found myself coding when I was a sophomore in high school. As a
-					cyclist binging too many British cycling YouTubers I found myself
-					frequently Googling "convert lbs to kg" and "miles to km" creating an
-					Android widget to do these <i>complex</i>
-					calculations. Eventually, I got it working on my phone and never published
-					it, but I was proud!
-				</p>
-				<p>
-					After college I found my first gig as a frontend developer. From day
-					one I was amazed at how big of an application it was. My Largest
-					projects in college were about 3000 lines with 3 other students, so I
-					struggled to wrap my head around a project written by 2 people that
-					could be ~300k lines. Each week was a water hose, I was drowning.
-					Every time I felt like I had it all together it was time to take
-					another step forward to realize I had only just begun. Welcome to
-					being a software engineer.
-				</p>
-				<p>
-					When not hacking away, you can find me cycling, not enjoying running,
-					or playing the latest video games with my friends.
-				</p>
-			</section>
+    <!-- — -->
+    <main>
+      <section id="about">
+        <h2 class="section-title">About</h2>
+        <div>Hi I'm Nate, I'm a Software Engineer based out of Bend, OR.</div>
+        <p>
+          I found myself coding when I was a sophomore in high school. As a
+          cyclist binging too many British cycling YouTubers I found myself
+          frequently Googling "convert lbs to kg" and "miles to km" creating an
+          Android widget to do these <i>complex</i>
+          calculations. Eventually, I got it working on my phone and never published
+          it, but I was proud!
+        </p>
+        <p>
+          After college I found my first gig as a frontend developer. From day
+          one I was amazed at how big of an application it was. My Largest
+          projects in college were about 3000 lines with 3 other students, so I
+          struggled to wrap my head around a project written by 2 people that
+          could be ~300k lines. Each week was a water hose, I was drowning.
+          Every time I felt like I had it all together it was time to take
+          another step forward to realize I had only just begun. Welcome to
+          being a software engineer, I love it.
+        </p>
+        <p>
+          I am an avid road cyclist questioning if I should get into mountain biking.
+					I run, but don't always enjoy the training. Other than that I am catching up 
+					on shows/movies that I should've watched earlier in life, or playing video games.
+        </p>
+      </section>
 
-			<section id="experience">
-				<h2 class="section-title">Experience</h2>
-				<a href="https://nrfcloud.com" target="_blank"> </a>
-				<div class="job-container">
-					<h1>Nordic Semiconductor</h1>
-					<div class="title">Senior Frontend Engineer</div>
-					<div class="date">2021 — 2024</div>
-					<div class="description">
-						Designed and built many key components and workflows for the IoT
-						Platform nRF Cloud >
+      <section id="experience">
+        <h2 class="section-title">Experience</h2>
+				<a href="https://nrfcloud.com" target="_blank" rel="noreferrer noopener">
+        <div class="job-container">
+            <div class="title-row">
+              <h1>Nordic Semiconductor</h1>
+              <div class="date">
+                2021 — 2024
+                <svg viewBox="0 0 16 16" width="1.2em" height="1.2em">
+									<path
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    d="M10.604 1h4.146a.25.25 0 0 1 .25.25v4.146a.25.25 0 0 1-.427.177L13.03 4.03L9.28 7.78a.75.75 0 0 1-1.06-1.06l3.75-3.75l-1.543-1.543A.25.25 0 0 1 10.604 1zM3.75 2A1.75 1.75 0 0 0 2 3.75v8.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0 0 14 12.25v-3.5a.75.75 0 0 0-1.5 0v3.5a.25.25 0 0 1-.25.25h-8.5a.25.25 0 0 1-.25-.25v-8.5a.25.25 0 0 1 .25-.25h3.5a.75.75 0 0 0 0-1.5h-3.5z"
+                 />
+								</svg>
+              </div>
+            </div>
+            <div class="title">Senior Frontend Engineer</div>
+            <div class="description">
+              I started my career working at Nordic Semiconductor on the IoT
+              platform nRF Cloud. When I started the team was about 10 people and grew to
+							nearly 30 developers across the world. I built and maintained components used
+							throughout the app. Collaborated with firmware, backend, and project managers
+							to create user experiences for IoT device fleet managers. 
+            </div>
+            <div class="stacks">
+              <div class="stack react">React</div>
+              <div class="stack typescript">Typescript</div>
+              <div class="stack redux">Redux</div>
+              <div class="stack graphql">Graphql</div>
+            </div>
 					</div>
-					<div class="stacks">
-						<div class="stack">React</div>
-						<div class="stack">Typescript</div>
-						<div class="stack">Redux</div>
-						<div class="stack">Graphql</div>
-					</div>
-				</div>
-			</section>
-			<section id="projects">
-				<h2 class="section-title">Projects</h2>
-				<div class="projects-container">
-					<div class="codewithfriends">
-						<h1>Jira Clone</h1>
-						<div class="date">2024</div>
-						<div class="description">
-							This project is something silly that I thought up to learn more
-							about
-							<strong>Next js</strong> and <strong>Vercel</strong>. I mostly set
-							out with the plan to copy Jira but coming up with a better way to
-							automate <i>standup's</i>.
-							<div class="stacks">
-								<div class="stack">Next</div>
-								<div class="stack">Vercel</div>
-								<div class="stack">React</div>
-								<div class="stack">Python</div>
+				</a>
+      </section>
+      <section id="projects">
+        <h2 class="section-title">Projects</h2>
+				<a href="https://www.codewithfriends.com" target="_blank" rel="noreferrer noopener">
+          <div class="job-container">
+						<div class="title-row">
+							<h1>Code With Friends</h1>
+							<div class="date">
+								2024
+								<svg viewBox="0 0 16 16" width="1.2em" height="1.2em">
+									<path
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    d="M10.604 1h4.146a.25.25 0 0 1 .25.25v4.146a.25.25 0 0 1-.427.177L13.03 4.03L9.28 7.78a.75.75 0 0 1-1.06-1.06l3.75-3.75l-1.543-1.543A.25.25 0 0 1 10.604 1zM3.75 2A1.75 1.75 0 0 0 2 3.75v8.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0 0 14 12.25v-3.5a.75.75 0 0 0-1.5 0v3.5a.25.25 0 0 1-.25.25h-8.5a.25.25 0 0 1-.25-.25v-8.5a.25.25 0 0 1 .25-.25h3.5a.75.75 0 0 0 0-1.5h-3.5z"/>
+								</svg>
 							</div>
 						</div>
-					</div>
-					<div class="codewithfriends">
-						<h1>Code With Friends</h1>
-						<div class="date">2024</div>
-						<div class="description">
-							In collaboration with a friend, we initiated a side project
-							focused on mutual skill development. The primary objective is to
-							showcase contributions to projects involving fellow software
-							engineers in Portland, fostering a community of continuous
-							learning and improvement.
-						</div>
-					</div>
-				</div>
-			</section>
-		</main>
-	</div>
+            <div class="description">
+              In collaboration with a friend, we initiated a side project
+              focused on mutual skill development. The primary objective is to
+              showcase contributions to projects involving fellow software
+              engineers in Portland, fostering a community of continuous
+              learning and improvement.
+            </div>
+            <div class="stacks">
+              <div class="stack react">React</div>
+              <div class="stack javascript">Javascript</div>
+            </div>
+          </div>
+					</a>
+        <div class="projects-container">
+          <div class="job-container">
+            <div class="title-row">
+              <h1>Jira Clone</h1>
+              <div class="date">
+                2024
+              </div>
+            </div>
+            <div class="description">
+              This project is something silly that I thought up to learn more
+              about
+              <strong>Next js</strong> and <strong>Vercel</strong>. I mostly set
+              out with the plan to copy Jira but coming up with a better way to
+              automate <i>standup's</i>.
+              <div class="stacks">
+                <div class="stack next">Next</div>
+                <div class="stack vercel">Vercel</div>
+                <div class="stack react">React</div>
+                <div class="stack python">Python</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  </div>
 </div>
 
 <style>
-	.name {
-		font-family: cursive;
-	}
-	.icon {
-		height: 44px;
-		aspect-ratio: 1;
-	}
-	.section-title {
-		display: none;
-		@media (max-width: 600px) {
-			display: block;
-			margin-bottom: 1rem;
-			border-bottom: 1px solid white;
-		}
-	}
+  .icon {
+    height: 44px;
+    aspect-ratio: 1;
+  }
+  .section-title {
+    display: none;
+    @media (max-width: 600px) {
+      display: block;
+      margin-bottom: 1rem;
+      border-bottom: 1px solid white;
+    }
+  }
 
-	.container {
-		margin-left: auto;
-		margin-right: auto;
+  .container {
+    margin-left: auto;
+    margin-right: auto;
 
-		max-width: 1080px;
-		@media (max-width: 900px) {
-			padding-left: 2rem;
-			padding-right: 2rem;
-		}
-	}
-	.wrapper {
-		display: flex;
-		justify-content: space-between;
-		gap: 3rem;
-		@media (max-width: 600px) {
-			flex-direction: column;
-		}
-	}
-	header {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		padding-top: 6rem;
+    max-width: 1080px;
+    @media (max-width: 900px) {
+      padding-left: 2rem;
+      padding-right: 2rem;
+    }
+  }
+  .wrapper {
+    display: flex;
+    justify-content: space-between;
+    gap: 3rem;
+    @media (max-width: 600px) {
+      flex-direction: column;
+    }
+  }
+  header {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-top: 6rem;
 
-		@media (max-width: 600px) {
+    @media (max-width: 600px) {
+    }
+    @media (min-width: 601px) and (max-width: 900px) {
+      /* Styles for tablets */
+    }
+    @media (min-width: 901px) {
+      position: sticky;
+      max-height: 70vh;
+      top: 0px;
+      flex-basis: 40%;
+      padding-bottom: 6rem;
+    }
+  }
+  main {
+    flex-basis: 60%;
+    padding-top: 6rem;
+    @media (max-width: 900px) {
+      padding-top: 2rem;
+    }
+  }
+  section {
+    width: 100%;
+    margin-bottom: 6rem;
+    scroll-margin-top: 3rem;
+  }
+  nav {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    @media (max-width: 600px) {
+      display: none;
+    }
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+		:visited {
+			text-decoration: none;
+			color: inherit;
 		}
-		@media (min-width: 601px) and (max-width: 900px) {
-			/* Styles for tablets */
-		}
-		@media (min-width: 901px) {
-			position: sticky;
-			max-height: 70vh;
-			top: 0px;
-			flex-basis: 40%;
-			padding-bottom: 6rem;
-		}
-	}
-	main {
-		flex-basis: 60%;
-		padding-top: 6rem;
-		@media (max-width: 900px) {
-			padding-top: 2rem;
-		}
-	}
-	section {
-		width: 100%;
-		margin-bottom: 6rem;
-		scroll-margin-top: 3rem;
-	}
-	nav {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-		@media (max-width: 600px) {
-			display: none;
-		}
-	}
-	a {
-		color: inherit;
-		text-decoration: none;
-		font-weight: bold;
-		width: fit-content;
-	}
-	a:visited {
-		text-decoration: none;
-		color: inherit;
-	}
-	a:active {
-		border: 1px solid goldenrod;
-	}
-	.section-link::after {
-		transition: width 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
-		content: "";
-		height: 1px;
-		width: 0px;
-		position: relative;
-		display: block;
-		background-color: black;
-	}
-	.section-link:hover::after {
-		width: 100%;
-	}
+  }
+  .section-link {
+    font-weight: bold;
+    width: fit-content;
+  }
+  .section-link::after {
+    transition: width 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
+    content: "";
+    height: 1px;
+    width: 0px;
+    position: relative;
+    display: block;
+    background-color: black;
+  }
+  .section-link:hover::after {
+    width: 100%;
+  }
 
-	.codewithfriends {
-		margin-bottom: 3rem;
-	}
+  .job-container {
+    padding: 1rem;
+    border-radius: 0.25rem;
+    transition: background-color 0.2s ease-in;
+  }
+  .job-container:hover {
+    outline: 1px solid #161e30;
+    background-color: #161e30;
+  }
 
-	.stacks {
-		display: flex;
-		gap: 8px;
-	}
-	.stack {
-		padding: 0.25rem;
-		border: 2px solid black;
-		color: white;
-		border-radius: 3px;
-		font-weight: bold;
-		background-color: linear-gradient(
-			90deg,
-			rgba(63, 94, 251, 1) 0%,
-			rgba(252, 70, 107, 1) 100%
-		);
-		width: fit-content;
-		transform: skewX(-25deg);
-	}
+  .title-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .stacks {
+    margin-top: 1rem;
+    display: flex;
+    gap: 8px;
+  }
+  .stack {
+    padding: 0.45rem;
+    border: 1px solid black;
+    border-radius: 45px;
+  }
+  .typescript {
+    background-color: #3178c6;
+  }
+  .react {
+    background-color: black;
+    color: #5dc6dd;
+  }
+  .javascript {
+    background-color: #f7e025;
+    color: black;
+  }
+  .graphql {
+    background-color: #e63bae;
+  }
+  .next {
+    background-color: black;
+  }
+  .vercel {
+    background-color: black;
+  }
+  .python {
+    background-color: #39719c;
+    color: #ffce41;
+  }
 </style>
